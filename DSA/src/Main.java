2d6cf5df5f960446f8f76fca5a4b7c85a8289314
shiven-puri <shiven.puri@proton.me>
2025-09-02 15:42:41 +0530

    first commit

--- DSA/src/Main.java
+++ DSA/src/Main.java
@@ -5,6 +5,9 @@ public class Main {
         list.insert(5);
         list.insert(6);
         list.insert(7);
+        list.InsertAtHead(9);
+        list.insertAt(22,2);
+        list.delete(0);
 
         list.show();
     }
--- DSA/src/linkedlist.java
+++ DSA/src/linkedlist.java
@@ -22,5 +22,39 @@ public class linkedlist {
             n = n.next;
         }
         System.out.println(n.data);
+
+    }
+    public void InsertAtHead(int data){
+        Node node = new Node();
+        node.data = data;
+        node.next = head;
+        head = node;
+    }
+    public void insertAt(int data, int index){
+        Node node = new Node();
+        node.data = data;
+        node.next = null;
+        if(index == 0){
+            InsertAtHead(data);
+        }
+        else{
+            Node n = head;
+            for(int i = 0; i < index - 1; i++){
+                n = n.next;
+            }
+            node.next = n.next;
+            n.next = node;
+        }
+    }
+    public void delete(int index) {
+        Node n = head;
+        if (index == 0) {
+            head = head.next;
+        } else {
+            for (int i = 0; i < index - 1; i++) {
+                n = n.next;
+            }
+            n.next = n.next.next;
+        }
     }
 }
